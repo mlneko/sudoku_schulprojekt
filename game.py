854fc9c003f334ce5d3b9f90381ffc221ea5178c
Ivan Khilchevskyi <ivan.khilchevskyi@sn-invent.de>
2024-03-13 12:13:23 +0100

    delete number, number with 0 or - or backspace delete, check solution, and other things

--- game.py
+++ game.py
@@ -7,6 +7,27 @@ screen = pygame.display.set_mode((500, 600))
 
 run = True
 
+def wait_on_user_key():
+    k = False
+    while True:
+        for event in pygame.event.get():
+            if event.type in [pygame.KEYUP, pygame.MOUSEBUTTONUP]:
+                k = True
+            break
+        if k:
+            break
+
+def draw_false_numbers(value, solution):
+    screen.fill((255, 255, 255))
+    draw_grid()
+    draw_numbers(value, [-1, 0])
+    for row in range(9):
+        for number in range(9):
+            if value[row][number] != 0 and solution[row][number] != value[row][number]:
+                pygame.draw.rect(screen, (255, 0, 0), pygame.Rect((number+1)*50-25, (row+1)*50-25, 50, 50), 3, 0)
+    pygame.display.update()
+                
+
 def generate_board():
     res = requests.get(("https://sudoku-api.vercel.app/api/dosuku?query={newboard(limit:1){grids{value,solution}}}")).json()['newboard']['grids'][0]
     return res['value'], res['solution']
@@ -23,7 +44,7 @@ def draw_numbers(numbers, selected_value):
     font = pygame.font.Font(None, 40)
     for row in range(9):
         for number in range(9):
-            text = font.render(str(numbers[row][number]), True, (0, 0, 0))
+            text = font.render(str(numbers[row][number]) if numbers[row][number] != 0 else "", True, (0, 0, 0))
             rect = text.get_rect(center=((number+1)*50, (row+1)*50))
             screen.blit(text, rect)
             if [number, row] == selected_value:
@@ -55,13 +76,10 @@ while run:
         elif event.type == pygame.MOUSEBUTTONUP:
             if reset_button.collidepoint(event.pos):
                 value, solution = generate_board()
-                selected_number = [-1, 0]
+                selected_number = [0, 0]
             elif check_entries_button.collidepoint(event.pos):
-                screen.fill((255, 255, 255))
-                draw_grid()
-                draw_numbers(solution, [-1, 0])
-                pygame.display.update()
-                pygame.time.wait(3000)
+                draw_false_numbers(value, solution)
+                wait_on_user_key()
                 draw_numbers(value, selected_number)
             elif 475 >= event.pos[0] >= 25 and 475 >= event.pos[1] >= 25:
                 selected_number = [int((event.pos[0]-25)/50),int((event.pos[1]-25)/50)]
@@ -76,7 +94,9 @@ while run:
                 selected_number[0] -= 1
             elif event.key == pygame.K_RIGHT and selected_number[0] < 9:
                 selected_number[0] += 1
-        elif event.type == pygame.TEXTINPUT and event.text in [str(i) for i in range(1, 10)]:
+        elif (event.type == pygame.TEXTINPUT and event.text in [str(i) for i in range(10)] + ["-"]) or (event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE):
+            if (event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE) or event.text == "-":
+                event.text = "0"
             value[selected_number[1]][selected_number[0]] = int(event.text)
     pygame.display.update() 
 
